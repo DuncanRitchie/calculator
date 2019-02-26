5220a838916ebd1df858ad6cb0dbb14509ba165a
Duncan Ritchie <codenation@Codes-MBP.lan>
2019-02-26 17:16:13 +0000

    Display now shows Id of button clicked.

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ class App extends Component {
     lastOperation: "Plus"
   }
   clickButton = (event) => {
-    
+    this.setState({total:event.target.id})
+    console.log(event.target.id)
     // this.setState({ total: event.target.id })
   }
-
   
   render() {
     return (
@@ -43,33 +43,33 @@ const Display = (props) => {
 const Buttons = (props) => {
   return (
     <div className="buttons">
-      <Button id="AC" innerHTML="AC"/>
-      <Button id="PlusMinus" innerHTML="&plusmn;"/>
-      <Button id="Percent" innerHTML="%"/>
-      <Button id="Divide" innerHTML="&divide;"/>
-      <Button id="Button7" innerHTML="7"/>
-      <Button id="Button8" innerHTML="8"/>
-      <Button id="Button9" innerHTML="9"/>
-      <Button id="Times" innerHTML="&times;"/>
-      <Button id="Button4" innerHTML="4"/>
-      <Button id="Button5" innerHTML="5"/>
-      <Button id="Button6" innerHTML="6"/>
-      <Button id="Minus" innerHTML="-"/>
-      <Button id="Button1" innerHTML="1"/>
-      <Button id="Button2" innerHTML="2"/>
-      <Button id="Button3" innerHTML="3"/>
-      <Button id="Plus" innerHTML="+"/>
-      <Button id="Button0" innerHTML="0"/>
-      <Button id="Point" innerHTML="."/>
-      <Button id="SquareRoot" innerHTML="&radic;"/>
-      <Button id="Equals" innerHTML="="/>
+      <Button id="AC" innerHTML="AC" clickButton={props.clickButton}/>
+      <Button id="PlusMinus" innerHTML="&plusmn;" clickButton={props.clickButton}/>
+      <Button id="Percent" innerHTML="%" clickButton={props.clickButton}/>
+      <Button id="Divide" innerHTML="&divide;" clickButton={props.clickButton}/>
+      <Button id="Button7" innerHTML="7" clickButton={props.clickButton}/>
+      <Button id="Button8" innerHTML="8" clickButton={props.clickButton}/>
+      <Button id="Button9" innerHTML="9" clickButton={props.clickButton}/>
+      <Button id="Times" innerHTML="&times;" clickButton={props.clickButton}/>
+      <Button id="Button4" innerHTML="4" clickButton={props.clickButton}/>
+      <Button id="Button5" innerHTML="5" clickButton={props.clickButton}/>
+      <Button id="Button6" innerHTML="6" clickButton={props.clickButton}/>
+      <Button id="Minus" innerHTML="-" clickButton={props.clickButton}/>
+      <Button id="Button1" innerHTML="1" clickButton={props.clickButton}/>
+      <Button id="Button2" innerHTML="2" clickButton={props.clickButton}/>
+      <Button id="Button3" innerHTML="3" clickButton={props.clickButton}/>
+      <Button id="Plus" innerHTML="+" clickButton={props.clickButton}/>
+      <Button id="Button0" innerHTML="0" clickButton={props.clickButton}/>
+      <Button id="Point" innerHTML="." clickButton={props.clickButton}/>
+      <Button id="SquareRoot" innerHTML="&radic;" clickButton={props.clickButton}/>
+      <Button id="Equals" innerHTML="=" clickButton={props.clickButton}/>
     </div>
   )
 }
 
 const Button = (props) => {
   return (
-    <div className="button" id={props.id} onClick={App.clickButton}>
+    <div className="button" id={props.id} onClick={props.clickButton}>
       <div>
         {props.innerHTML}
       </div>
