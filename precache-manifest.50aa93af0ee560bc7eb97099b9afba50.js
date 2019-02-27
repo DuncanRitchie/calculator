7f740a83cea86dedd24ebaa32bc57ee7f7794f84
Duncan Ritchie <codenation@Codes-MBP.lan>
2019-02-27 17:23:57 +0000

    Updates

---
+++ precache-manifest.50aa93af0ee560bc7eb97099b9afba50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2a97d8d426c7554df3f",
+    "url": "/Calculator/static/js/runtime~main.c2a97d8d.js"
+  },
+  {
+    "revision": "ab5536b42caec229c9b7",
+    "url": "/Calculator/static/js/main.ab5536b4.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/Calculator/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "ab5536b42caec229c9b7",
+    "url": "/Calculator/static/css/main.3fa07fbf.chunk.css"
+  },
+  {
+    "revision": "c228d0c2606a3333a98c102a982606e8",
+    "url": "/Calculator/index.html"
+  }
+];
